58a9853ac9bdd87ea6ff2102134ae5cab8667c2f
Brandon Weng <wengbrandon@gmail.com>
2022-09-27 22:37:31 -0700

    Use blocking and signalling

---
+++ types/accesscontrol/access_operation_map.go
@@ -0,0 +1,24 @@
+package accesscontrol
+
+// Alias for Map of MessageIndex -> AccessOperation -> Channel
+type MessageAccessOpsChannelMapping = map[int]AccessOpsChannelMapping
+
+// Alias for Map of AccessOperation -> Channel
+type AccessOpsChannelMapping = map[*AccessOperation][]chan interface{}
+
+// Waits for all signals to complete before proceeding
+func WaitForSignals(accessOpsToChannelsMap AccessOpsChannelMapping) {
+	for _, channels := range accessOpsToChannelsMap {
+		for _, channel := range channels {
+			<-channel
+		}
+	}
+}
+
+func SendSignals(accessOpsToChannelsMap AccessOpsChannelMapping) {
+	for accessOp, channels := range accessOpsToChannelsMap {
+		for _, channel := range channels {
+			channel <- accessOp
+		}
+	}
+}

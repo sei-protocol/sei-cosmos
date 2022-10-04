ad1c41887fe48becbd7b856dea1afe4c318cd9b4
Brandon Weng <wengbrandon@gmail.com>
2022-10-04 14:08:46 -0700

    Add ResourceId Getter for AccessOperation

--- types/accesscontrol/access_operation.go
+++ types/accesscontrol/access_operation.go
@@ -1,9 +1,10 @@
 package accesscontrol
 
-import (
-	fmt "fmt"
-)
-
-func (a *AccessOperation) GetResourceIDTemplate(args []any) string {
-	return fmt.Sprintf(a.GetIdentifierTemplate(), args...)
+func (a *AccessOperation) GetResourceID(msg interface{}) string {
+	// If the Msg implements GetMsgResourceIdentifier, then use that to retrieve the identifier, or else
+	// use the Identifier template as the resourceId for this operation
+	if message, ok := msg.(interface{GetMsgResourceIdentifier(a AccessOperation) string}); ok {
+		return message.GetMsgResourceIdentifier(*a)
+	}
+	return a.GetIdentifierTemplate()
 }

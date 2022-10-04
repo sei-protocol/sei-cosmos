c7eaa817ea024f264f9d95aa784721743d59d164
Brandon Weng <wengbrandon@gmail.com>
2022-10-03 23:09:55 -0700

    Add gov proposal handler for acl

---
+++ x/accesscontrol/client/cli/tx.go
@@ -0,0 +1,81 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/x/accesscontrol/client/utils"
+	"github.com/cosmos/cosmos-sdk/x/accesscontrol/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+
+	"github.com/spf13/cobra"
+)
+
+func GetTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	updateResourceDependencyMappingProposalCmd := MsgUpdateResourceDependencyMappingProposalCmd()
+	flags.AddTxFlagsToCmd(updateResourceDependencyMappingProposalCmd)
+
+	cmd.AddCommand(
+		updateResourceDependencyMappingProposalCmd,
+	)
+
+	return cmd
+}
+
+func MsgUpdateResourceDependencyMappingProposalCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-resource-dependency-mapping [proposal-file]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Submit an UpdateResourceDependencyMapping proposal",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			println("MsgUpdateResourceDependencyMappingProposalCmd:HANDLING CLI REQUEST")
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			proposal, err := utils.ParseMsgUpdateResourceDependencyMappingProposalFile(clientCtx.Codec, args[0])
+			if err != nil {
+				return err
+			}
+
+			println("MsgUpdateResourceDependencyMappingProposalCmd:GETTING FROM ADDRESS")
+			from := clientCtx.GetFromAddress()
+
+			content := types.MsgUpdateResourceDependencyMappingProposal{
+				Title:	proposal.Title,
+				Description: proposal.Description,
+				MessageDependencyMapping: proposal.MessageDependencyMapping,
+			}
+
+			deposit, err := sdk.ParseCoinsNormalized(proposal.Deposit)
+			if err != nil {
+				println("Unable to prase coin from UpdateResourceDependencyMapping from CLI")
+				return err
+			}
+
+			println("MsgUpdateResourceDependencyMappingProposalCmd:SUBMITTING PROPOSAL")
+			msg, err := govtypes.NewMsgSubmitProposal(&content, deposit, from)
+			if err != nil {
+				println("Unable to submit proposal for UpdateResourceDependencyMapping from CLI")
+				return err
+			}
+			println("MsgUpdateResourceDependencyMappingProposalCmd:SENDING!")
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	return cmd
+}

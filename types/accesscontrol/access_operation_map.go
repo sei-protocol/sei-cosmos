7af23d9ff0a485ef83537ce17fde54a1f592c580
Brandon Weng <wengbrandon@gmail.com>
2022-10-07 00:02:13 -0700

    Move Parallel TX signalling to sei-chain

--- types/accesscontrol/access_operation_map.go
+++ types/accesscontrol/access_operation_map.go
@@ -6,18 +6,22 @@ type MessageAccessOpsChannelMapping = map[int]AccessOpsChannelMapping
 // Alias for Map of AccessOperation -> Channel
 type AccessOpsChannelMapping = map[AccessOperation][]chan interface{}
 
-func WaitForAllSignals(accessOpsToChannelsMap AccessOpsChannelMapping) {
-	for _, channels := range accessOpsToChannelsMap {
-		for _, channel := range channels {
-			<-channel
+func SendAllSignals(txIndex int, messageIndexToAccessOpsChannelMapping MessageAccessOpsChannelMapping) {
+	for _, accessOpsToChannelsMap  := range messageIndexToAccessOpsChannelMapping {
+		for _, channels := range accessOpsToChannelsMap {
+			for _, channel := range channels {
+				channel <- struct{}{}
+			}
 		}
 	}
 }
 
-func SendAllSignals(accessOpsToChannelsMap AccessOpsChannelMapping) {
-	for _, channels := range accessOpsToChannelsMap {
-		for _, channel := range channels {
-			channel <- struct{}{}
+func WaitForAllSignals(txIndex int, messageIndexToAccessOpsChannelMapping MessageAccessOpsChannelMapping) {
+	for _, accessOpsToChannelsMap  := range messageIndexToAccessOpsChannelMapping {
+		for _, channels := range accessOpsToChannelsMap {
+			for _, channel := range channels {
+				<-channel
+			}
 		}
 	}
 }

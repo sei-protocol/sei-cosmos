5cf5b36ffa7a6e2e44c761e9e339829a743a5771
Uday Patil <udpatil@gmail.com>
2022-10-09 19:57:31 -0700

    Ante deps (#41)
    ## Describe your changes and provide context
    This introduces an ante dependency decorator and an interface by which
    we can define the dependencies for ante handlers.
    
    ## Testing performed to validate your change
    Needs testing with the related sei-chain change

--- baseapp/baseapp.go
+++ baseapp/baseapp.go
@@ -62,12 +62,13 @@ type BaseApp struct { //nolint: maligned
 	interfaceRegistry types.InterfaceRegistry
 	txDecoder         sdk.TxDecoder // unmarshal []byte into sdk.Tx
 
-	anteHandler            sdk.AnteHandler  // ante handler for fee and auth
-	initChainer            sdk.InitChainer  // initialize state with validators and state blob
-	beginBlocker           sdk.BeginBlocker // logic to run before any txs
-	endBlocker             sdk.EndBlocker   // logic to run after all txs, and to determine valset changes
-	addrPeerFilter         sdk.PeerFilter   // filter peers by address and port
-	idPeerFilter           sdk.PeerFilter   // filter peers by node ID
+	anteHandler            sdk.AnteHandler      // ante handler for fee and auth
+	anteDepGenerator       sdk.AnteDepGenerator // ante dep generator for parallelization
+	initChainer            sdk.InitChainer      // initialize state with validators and state blob
+	beginBlocker           sdk.BeginBlocker     // logic to run before any txs
+	endBlocker             sdk.EndBlocker       // logic to run after all txs, and to determine valset changes
+	addrPeerFilter         sdk.PeerFilter       // filter peers by address and port
+	idPeerFilter           sdk.PeerFilter       // filter peers by node ID
 	prepareProposalHandler sdk.PrepareProposalHandler
 	processProposalHandler sdk.ProcessProposalHandler
 	finalizeBlocker        sdk.FinalizeBlocker

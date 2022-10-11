a67ef22a3ea32261e1d280ee13569e7b2d1b1681
Brandon Weng <wengbrandon@gmail.com>
2022-10-10 22:25:25 -0700

    Revert "Fix accesscontrol ante-deps (#42)"
    This reverts commit e532576ad8f2b691ad00bcbec848f532bf6c980f.

--- types/handler_test.go
+++ types/handler_test.go
@@ -24,14 +24,13 @@ func (s *handlerTestSuite) SetupSuite() {
 
 func (s *handlerTestSuite) TestChainAnteDecorators() {
 	// test panic
-	s.Require().Nil(sdk.ChainAnteDecorators([]sdk.AnteFullDecorator{}...))
+	s.Require().Nil(sdk.ChainAnteDecorators([]sdk.AnteDecorator{}...))
 
 	ctx, tx := sdk.Context{}, sdk.Tx(nil)
 	mockCtrl := gomock.NewController(s.T())
 	mockAnteDecorator1 := mocks.NewMockAnteDecorator(mockCtrl)
 	mockAnteDecorator1.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, gomock.Any()).Times(1)
-	handler, _ := sdk.ChainAnteDecorators(sdk.DefaultWrappedAnteDecorator(mockAnteDecorator1))
-	_, err := handler(ctx, tx, true)
+	_, err := sdk.ChainAnteDecorators(mockAnteDecorator1)(ctx, tx, true)
 	s.Require().NoError(err)
 
 	mockAnteDecorator2 := mocks.NewMockAnteDecorator(mockCtrl)
@@ -41,10 +40,8 @@ func (s *handlerTestSuite) TestChainAnteDecorators() {
 	mockAnteDecorator1.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, gomock.Any()).Times(1)
 	mockAnteDecorator2.EXPECT().AnteHandle(gomock.Eq(ctx), gomock.Eq(tx), true, gomock.Any()).Times(1)
 
-	handler, _ = sdk.ChainAnteDecorators(
-		sdk.DefaultWrappedAnteDecorator(mockAnteDecorator1),
-		sdk.DefaultWrappedAnteDecorator(mockAnteDecorator2),
-	)
-	_, err = handler(ctx, tx, true)
+	_, err = sdk.ChainAnteDecorators(
+		mockAnteDecorator1,
+		mockAnteDecorator2)(ctx, tx, true)
 	s.Require().NoError(err)
 }

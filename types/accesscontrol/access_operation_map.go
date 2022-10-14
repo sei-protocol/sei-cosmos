693d10638cfe593851609a3aaf15288918fad8a6
Brandon Weng <wengbrandon@gmail.com>
2022-10-14 11:11:39 -0400

    Add AnteHandler access ops and move completion channel signalling

--- types/accesscontrol/access_operation_map.go
+++ types/accesscontrol/access_operation_map.go
@@ -7,19 +7,25 @@ type MessageAccessOpsChannelMapping = map[int]AccessOpsChannelMapping
 type AccessOpsChannelMapping = map[AccessOperation][]chan interface{}
 
 func WaitForAllSignalsForTx(messageIndexToAccessOpsChannelMapping MessageAccessOpsChannelMapping) {
+	println("ProcessTxConcurrent::waiting for Signal Tx")
 	for _, accessOpsToChannelsMap  := range messageIndexToAccessOpsChannelMapping {
 		for _, channels := range accessOpsToChannelsMap {
 			for _, channel := range channels {
 				<-channel
+				println("ProcessTxConcurrent::recieved one signal")
+			}
+		}
+	}
+	println("ProcessTxConcurrent::recieved all signals")
+}
+
+func SendAllSignalsForTx(messageIndexToAccessOpsChannelMapping MessageAccessOpsChannelMapping) {
+	for _, accessOpsToChannelsMap  := range messageIndexToAccessOpsChannelMapping {
+		for _, channels := range accessOpsToChannelsMap {
+			for _, channel := range channels {
+				channel <- struct{}{}
 			}
 		}
 	}
 }
 
-func SendAllSignalsForMsg(accessOpsChannelMapping AccessOpsChannelMapping) {
-	for _, channels := range accessOpsChannelMapping {
-		for _, channel := range channels {
-			channel <- struct{}{}
-		}
-	}
-}

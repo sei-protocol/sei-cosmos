e8b419a0d431ada52445ae83b8f74973afa01432
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2022-10-27 10:23:50 -0400

    unit tests

--- types/accesscontrol/validation.go
+++ types/accesscontrol/validation.go
@@ -90,13 +90,17 @@ func ValidateAccessOperations(accessOps []AccessOperation, events []abci.Event) 
 	accessOpsComparators := BuildComparatorFromAccessOp(accessOps)
 
 	missingAccessOps := make(map[Comparator]bool)
+	println(len(eventsComparators))
 
 	// If it's using default synchronous access op mapping then no need to verify
 	if IsDefaultSynchronousAccessOps(accessOps) {
 		return missingAccessOps
 	}
 
+	println(len(eventsComparators))
+
 	for _, eventComparator := range eventsComparators {
+		println("EVENT")
 		if eventComparator.IsConcurrentSafeIdentifier() {
 			continue
 		}
@@ -110,6 +114,7 @@ func ValidateAccessOperations(accessOps []AccessOperation, events []abci.Event) 
 		}
 
 		if !matched {
+			println("NOT MATCHED")
 			missingAccessOps[eventComparator] = true
 		}
 

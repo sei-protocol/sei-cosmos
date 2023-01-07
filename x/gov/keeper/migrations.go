a76ea5a56a7d21580b58b257dc03b5ded3f4ac44
Yiming Zang <yzang@twitter.com>
2023-01-07 11:42:05 -0800

    Fix migration registration and version

--- x/gov/keeper/migrations.go
+++ x/gov/keeper/migrations.go
@@ -3,7 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v043 "github.com/cosmos/cosmos-sdk/x/gov/legacy/v043"
-	v4 "github.com/cosmos/cosmos-sdk/x/gov/legacy/v4"
+	v3 "github.com/cosmos/cosmos-sdk/x/gov/legacy/v3"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -23,10 +23,5 @@ func (m Migrator) Migrate1to2(ctx sdk.Context) error {
 
 // Migrate2to3 migrates from version 2 to 3.
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
-	return v043.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc)
-}
-
-// Migrate3to4 migrates from version 3 to 4.
-func (m Migrator) Migrate3to4(ctx sdk.Context) error {
-	return v4.MigrateStore(ctx, m.keeper.paramSpace)
+	return v3.MigrateStore(ctx, m.keeper.paramSpace)
 }

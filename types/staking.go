ced3eccf0f946dca7f1fa65b935463cbb364a50b
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2023-02-09 16:37:36 -0500

    Update staking.go

--- types/staking.go
+++ types/staking.go
@@ -10,9 +10,8 @@ const (
 	// Max percentage of the total voting power a single validator can have
 	DefaultMaxVotingPowerRatio = "0.1"
 
-	// Maximum amount of staking power we allow per validator
-	// before the max voting power ratio is enforced
-	// Treshold for number of token staked = DefaultPowerReduction * DefaultMaxVotingPowerEnforcementThreshold
+	// Maximum amount of staking power we allow per validator before the max voting power ratio is enforced
+	// Threshold for number of token staked = DefaultPowerReduction * DefaultMaxVotingPowerEnforcementThreshold
 	DefaultMaxVotingPowerEnforcementThreshold uint64 = 10000
 
 	// Delay, in blocks, between when validator updates are returned to the

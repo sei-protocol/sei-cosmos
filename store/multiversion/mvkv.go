552fb940bb8850a15b5a56d61ee2c255607f0ea8
Uday Patil <udpatil@gmail.com>
2024-01-25 16:50:44 -0600

    Fix mvkv to adhere to updated KVStore interface

--- store/multiversion/mvkv.go
+++ store/multiversion/mvkv.go
@@ -301,6 +301,14 @@ func (store *VersionIndexedStore) iterator(start []byte, end []byte, ascending b
 
 }
 
+func (v *VersionIndexedStore) VersionExists(version int64) bool {
+	return v.parent.VersionExists(version)
+}
+
+func (v *VersionIndexedStore) DeleteAll(start, end []byte) error {
+	return v.parent.DeleteAll(start, end)
+}
+
 // GetStoreType implements types.KVStore.
 func (v *VersionIndexedStore) GetStoreType() types.StoreType {
 	return v.parent.GetStoreType()

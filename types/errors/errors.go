b2a65f839fa602f7efddbd9d60f002283feeb5fa
yzang2019 <zymfrank@gmail.com>
2024-02-06 15:42:51 +0800

    Fix

--- types/errors/errors.go
+++ types/errors/errors.go
@@ -147,7 +147,7 @@ var (
 	// ErrInvalidConcurrency defines an error occurred during concurrent execution
 	ErrInvalidConcurrencyExecution = Register(RootCodespace, 41, "error during concurrent execution")
 
-	// ErrAlreadyExists defines an error for which the tx is considered as a spam because the node has already seen it before
+	// ErrAlreadyExists defines an error for which the tx failed checkTx because the node has already seen it before
 	ErrAlreadyExists = Register(RootCodespace, 42, "error tx already exists")
 
 	// ErrPanic is only set when we recover from a panic, so we know to

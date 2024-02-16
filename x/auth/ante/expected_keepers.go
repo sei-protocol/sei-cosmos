02d88d04eb419089fe7e4f3d149007de90e0ca91
Philip Su <philip.su.522@gmail.com>
2024-02-15 16:05:48 -0800

    Fix account keeper interface to use in EIP-712 impl

--- x/auth/ante/expected_keepers.go
+++ x/auth/ante/expected_keepers.go
@@ -9,10 +9,15 @@ import (
 // AccountKeeper defines the contract needed for AccountKeeper related APIs.
 // Interface provides support to use non-sdk AccountKeeper for AnteHandler's decorators.
 type AccountKeeper interface {
-	GetParams(ctx sdk.Context) (params types.Params)
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
-	SetAccount(ctx sdk.Context, acc types.AccountI)
+	NewAccountWithAddress(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
 	GetModuleAddress(moduleName string) sdk.AccAddress
+	GetAllAccounts(ctx sdk.Context) (accounts []types.AccountI)
+	IterateAccounts(ctx sdk.Context, cb func(account types.AccountI) bool)
+	GetSequence(sdk.Context, sdk.AccAddress) (uint64, error)
+	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
+	SetAccount(ctx sdk.Context, account types.AccountI)
+	RemoveAccount(ctx sdk.Context, account types.AccountI)
+	GetParams(ctx sdk.Context) (params types.Params)
 }
 
 // FeegrantKeeper defines the expected feegrant keeper.

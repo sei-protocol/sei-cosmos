389b8148df96066d2b5962d4367df6c8ba899c8b
Uday Patil <udpatil@gmail.com>
2024-03-18 16:34:09 -0500

    apply concurrency worker config by default

--- server/config/toml.go
+++ server/config/toml.go
@@ -105,7 +105,7 @@ num-orphan-per-file = {{ .BaseConfig.NumOrphanPerFile }}
 orphan-dir = "{{ .BaseConfig.OrphanDirectory }}"
 
 # concurrency-workers defines how many workers to run for concurrent transaction execution
-# concurrency-workers = {{ .BaseConfig.ConcurrencyWorkers }}
+concurrency-workers = {{ .BaseConfig.ConcurrencyWorkers }}
 
 # occ-enabled defines whether OCC is enabled or not for transaction execution
 occ-enabled = {{ .BaseConfig.OccEnabled }}

bff5c96d7e7a759fea819dc25ca2607ed29581fc
yzang2019 <zymfrank@gmail.com>
2024-08-09 11:43:35 -0700

    Fix log

--- x/auth/ante/setup.go
+++ x/auth/ante/setup.go
@@ -54,7 +54,7 @@ func (sud SetUpContextDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate
 	if cp := ctx.ConsensusParams(); cp != nil && cp.Block != nil {
 		// If there exists a maximum block gas limit, we must ensure that the tx
 		// does not exceed it.
-		fmt.Printf("[Debug] Checking if gas limit exceed or not, tx gas is %d while block gas limit is %d\n", gasTx.GetGas(), cp.Block.MaxGas)
+		fmt.Printf("[Debug] Checking if gas limit exceed or not, tx gas is %d while block gas limit is %d at height %d\n", gasTx.GetGas(), cp.Block.MaxGas, ctx.BlockHeight())
 		if cp.Block.MaxGas > 0 && gasTx.GetGas() > uint64(cp.Block.MaxGas) {
 			return newCtx, sdkerrors.Wrapf(sdkerrors.ErrInvalidGasLimit, "tx gas limit %d exceeds block max gas %d", gasTx.GetGas(), cp.Block.MaxGas)
 		}

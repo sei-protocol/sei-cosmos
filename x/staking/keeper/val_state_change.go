1b003736ceb119c9d23f77f4d44046b457a21395
Uday Patil <udpatil@gmail.com>
2025-12-08 18:02:55 -0600

    Add log

--- x/staking/keeper/val_state_change.go
+++ x/staking/keeper/val_state_change.go
@@ -27,6 +27,7 @@ func (k Keeper) Arctic1ValidatorHotfix(ctx sdk.Context) {
 	ctx.Logger().Info("Arctic-1 Hotfix: iterating over validators", "count", len(validators))
 	for _, validator := range validators {
 		if string(validator.Status) != types.BondStatusUnbonding {
+			ctx.Logger().Info("Arctic-1 Hotfix: validator not in unbonding status", "validator", validator.GetOperator().String(), "status", string(validator.Status))
 			continue
 		}
 		ctx.Logger().Info("Arctic-1 Hotfix: unjailing validator", "validator", validator.GetOperator().String())
